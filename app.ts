3c90ce4cb30a62fe80ab38c79f505e314ab2c97f
Vadim <ilovedjent@yandex.ru>
2024-03-18 19:03:11 +0300

    sm_sht

--- app.ts
+++ app.ts
@@ -2,8 +2,8 @@ import express from 'express'
 import cors from 'cors'
 import 'dotenv/config'
 import {
-    SearchRoutes, 
-    UsersRoutes, 
+    SearchRoutes,
+    UsersRoutes,
     BeersRoutes,
     ImagesRoutes
 } from '@/routes/index'
@@ -12,7 +12,7 @@ const app = express()
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(cors())
+app.use(cors());
 
 app.use('/api/users', UsersRoutes)
 app.use('/api/beers', BeersRoutes)
--- src/controllers/users/sign/logIn.ts
+++ src/controllers/users/sign/logIn.ts
@@ -1,18 +1,17 @@
 import jwt from 'jsonwebtoken';
 import bcrypt from 'bcrypt';
-import { User } from '@prisma/client';
 import { Request, Response } from 'express';
 import { prisma } from '@/prisma/script';
 import { UserLoginData } from '@/types/types';
 import { handleServerError } from '@/utils/handleServerError';
 import { logger } from '@/config/loggerConfig';
- 
+
 export const logIn = async (req: Request, res: Response) => {
     const { nickName, password }: UserLoginData = req.body
     try {
         const existingUser = await prisma.user.findFirst({
             where: { nickName }
-        }) 
+        })
         if (!existingUser) {
             // return Обязательно нужно добавить, чтобы после возвращения ответа код останавливался, а не продолжал слать headers
             return res.status(404).json({
@@ -27,7 +26,11 @@ export const logIn = async (req: Request, res: Response) => {
             });
         }
 
-        const token = jwt.sign({ userId: existingUser.id }, process.env.JWT_SECRET, { expiresIn: '30d' })
+        const token = jwt.sign(
+            { userId: existingUser.id },
+            process.env.JWT_SECRET,
+            { expiresIn: '30d' }
+        )
 
         logger.info(`${nickName} выполнил вход в приложение`)
 
